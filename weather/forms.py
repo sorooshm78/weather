9e87377b477c7748a347192d56d1519bce57ac63
Soroush mohammadi <sorooshm78@gmail.com>
2022-11-29 19:35:10 +0330

    initial index view

---
+++ weather/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from .models import City
+
+
+class CityModelForm(forms.ModelForm):
+	class Meta:
+		model = City
+		fields = [
+            'name',
+        ]
--- weather/views.py
+++ weather/views.py
@@ -1,17 +1,34 @@
-from django.views.generic import TemplateView
+import requests
+import json
+
+from django.views.generic import ListView, CreateView
+from django.urls import reverse
+
+from .models import City
+from .forms import CityModelForm
 
 # Create your views here.
-class Home(TemplateView):
+class ListCreateCityView(CreateView):
+    model = City
+    form_class = CityModelForm
     template_name = "weather/weather.html"
+
+    def __init__(self, *args, **kwargs):
+        self.api_key = self.get_api_key()
+
+    def get_api_key(self):
+        with open("secret.json", "r") as secret_file:
+            api_key = json.load(secret_file)["Key"]
+        return api_key
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["weather_data"] = [
-            {
-                "icon": "o",
-                "city": "tehran",
-                "temperature": 45,
-                "description": "tttt",
-            },
-        ]
+        
+        # response = requests.get()
+        for city in self.get_queryset():
+            print(city.name)
+
         return context
+    
+    def get_success_url(self):
+        return reverse('index')
