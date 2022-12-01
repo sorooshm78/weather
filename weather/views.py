d96d052fc24c5926315a0c2e5bb98c222193fb6e
Soroush mohammadi <sorooshm78@gmail.com>
2022-12-01 18:35:31 +0330

    refactor extract city weather data from view

--- weather/views.py
+++ weather/views.py
@@ -7,37 +7,61 @@ from django.urls import reverse
 from .models import City
 from .forms import CityModelForm
 
-# Create your views here.
-class ListCreateCityView(CreateView):
-    model = City
-    form_class = CityModelForm
-    template_name = "weather/weather.html"
 
-    def __init__(self, *args, **kwargs):
+class CityWeatherData:
+    def __init__(self, city_name):
+        self.city_name = city_name
         self.api_key = self.get_api_key()
-
-    def get_url(self, city_name):
-        url = "https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
-        return url.format(city_name=city_name, api_key=self.api_key)
+        self.url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={self.api_key}"
 
     def get_api_key(self):
         with open("secret.json", "r") as secret_file:
             api_key = json.load(secret_file)["Key"]
         return api_key
 
+    def city_exist(self):
+        try:
+            response = requests.get(self.url)
+        except BaseException:
+            raise ValueError("Connection Error")
+        else:
+            if response.status_code != 200:
+                raise ValueError("City Not Exist")
+
+    def get_json_data(self):
+        response = requests.get(self.url)
+        return response.json()
+
+    def get_data(self):
+        try:
+            data = self.get_json_data()
+            return {
+                "city": self.city_name,
+                "temperature": data["main"]["temp"],
+                "description": data["weather"][0]["description"],
+                "icon": data["weather"][0]["icon"],
+            }
+        except BaseException:
+            err = "Connection Error"
+            return {
+                "city": self.city_name,
+                "temperature": 0,
+                "description": err,
+            }
+
+
+# Create your views here.
+class ListCreateCityView(CreateView):
+    model = City
+    form_class = CityModelForm
+    template_name = "weather/weather.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         weather_data = []
         for city in self.get_queryset():
-            response = requests.get(self.get_url(city.name)).json()
-            weather_data.append(
-                {
-                    "city": city.name,
-                    "temperature": response["main"]["temp"],
-                    "description": response["weather"][0]["description"],
-                    "icon": response["weather"][0]["icon"],
-                }
-            )
+            weather_data.append(CityWeatherData(city.name).get_data())
+
         context["weather_data"] = weather_data
         return context
 
@@ -46,13 +70,13 @@ class ListCreateCityView(CreateView):
 
     def form_valid(self, form):
         city_name = form.cleaned_data["name"]
-        response = requests.get(self.get_url(city_name))
 
-        if response.status_code != 200:
-            form.add_error("name", "City is not exist")
+        try:
+            CityWeatherData(city_name).city_exist()
+            return super().form_valid(form)
+        except ValueError as err:
+            form.add_error("name", err)
             return self.form_invalid(form)
-
-        return super().form_valid(form)
 
 
 class DeleteCity(DeleteView):
