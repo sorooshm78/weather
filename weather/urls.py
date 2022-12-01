263247fcf6921599c65b2327d096d7ff34d84059
Soroush mohammadi <sorooshm78@gmail.com>
2022-12-01 19:23:53 +0330

    remove command line

--- weather/urls.py
+++ weather/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path("", views.ListCreateCityView.as_view(), name="index"),
     path("delete/<city_name>/", views.DeleteCity.as_view(), name="delete_city"),
-    # path("delete/<name>/", views.DeleteCity.as_view(), name="delete_city"),
 ]
