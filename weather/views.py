4ebed3c36eee7a91c0e878260d06c6243daffb7b
Soroush mohammadi <sorooshm78@gmail.com>
2022-12-02 19:22:56 +0330

    add thread for improve performance

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,5 @@
+from concurrent.futures import ThreadPoolExecutor
+
 from django.views.generic import CreateView, DeleteView
 from django.urls import reverse
 
@@ -6,7 +8,8 @@ from .forms import CityModelForm
 from .weather_data import WeatherData
 
 
-# Create your views here.
+MAX_THREAD_WORKERS = 10
+
 class ListCreateCityView(CreateView):
     model = City
     form_class = CityModelForm
@@ -27,14 +30,19 @@ class ListCreateCityView(CreateView):
                 "temperature": 0,
                 "description": err,
             }
-
+    
+    def get_context_weather_data(self, list_city):
+        with ThreadPoolExecutor(max_workers=MAX_THREAD_WORKERS) as pool:
+            weather_data = list(pool.map(self.get_city_weather_data, list_city))
+        
+        return weather_data
+        
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        weather_data = []
-        for city in self.get_queryset():
-            weather_data.append(self.get_city_weather_data(city.name))
-
-        context["weather_data"] = weather_data
+        
+        list_city = self.get_queryset()
+        context["weather_data"] = self.get_context_weather_data(list_city)
+        
         return context
 
     def get_success_url(self):
